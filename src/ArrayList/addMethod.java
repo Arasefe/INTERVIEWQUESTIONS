6731869b63ead2b54f751e9a6fe7df4c081f1506
Arasefe <arasefe@users.noreply.github.com>
2020-06-10 18:53:38 -0500

    Initial commit

---
+++ src/ArrayList/addMethod.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class addMethod {
+    public static void main(String[] args) {
+        ArrayList<String>list=new ArrayList<>();
+        list.add("Aras");
+        list.add(2,"Omer");
+        list.add(1,"Ismail");
+       // list.add("Aras");
+        System.out.println(list);
+    }
+}
