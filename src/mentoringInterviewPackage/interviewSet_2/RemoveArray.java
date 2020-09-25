211ed3aaa44396ee277939cf12ccea4e63323aff
Arasefe <ismail_yildirim69@yahoo.com>
2020-09-24 21:52:58 -0500

    24Sept2020

--- src/mentoringInterviewPackage/interviewSet_2/RemoveArray.java
+++ src/mentoringInterviewPackage/interviewSet_2/RemoveArray.java
@@ -1,6 +1,50 @@
 package mentoringInterviewPackage.interviewSet_2;
 
-public class RemoveArray {
-    //7.Write a removeAll() takes two parameters: an ArrayList of Strings called wordList, and a 	String called targetWord.So remove the targetWord from ArrayList.EX :ArrayList("hi","hey","hi","yo"));  ----> removeAll(wordList,"hi"); --->["hey","yo"]
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
 
+public class RemoveArray {
+    // Write a removeAll() takes two parameters: an ArrayList of Strings called wordList,
+    // and a String called targetWord.So remove the targetWord from ArrayList.EX :ArrayList("hi","hey","hi","yo"));  ----> removeAll(wordList,"hi"); --->["hey","yo"]
+
+    public static void main(String[] args) {
+
+        removeAll(new ArrayList<String>(Arrays.asList("Aras", "Tulpar", "Ismail", "Banu")), "Aras");
+        removeAll2(new ArrayList<String>(Arrays.asList("Aras", "Tulpar", "Ismail", "Banu")), "Aras");
+        removeAll3(new ArrayList<String>(Arrays.asList("Aras", "Tulpar", "Ismail", "Banu")), "Aras");
+
+    }
+
+    public static void removeAll(ArrayList<String> list, String targetWord) {
+
+        list.removeIf(p -> p.equals(targetWord));
+
+        System.out.println(list);
+    }
+
+    public static void removeAll2(ArrayList<String> list, String targetWord) {
+
+        Iterator<String> names = list.iterator();
+        while (names.hasNext()) {
+            String str=names.next();
+            if (str.equals(targetWord)) {
+                names.remove();
+            }
+        }
+        System.out.println(list);
+
+    }
+
+    public static void removeAll3(ArrayList<String> list, String targetWord){
+        ListIterator<String>names=list.listIterator();
+        while(names.hasNext()){
+            String str=names.next();
+            if(str.equals(targetWord)){
+                names.remove();
+            }
+        }
+        System.out.println(list);
+    }
 }
--- src/mentoringInterviewPackage/interviewSet_2/ShiftArrayElement.java
+++ src/mentoringInterviewPackage/interviewSet_2/ShiftArrayElement.java
@@ -2,4 +2,14 @@ package mentoringInterviewPackage.interviewSet_2;
 
 public class ShiftArrayElement {
     // Shift the first element of the array to the end and keep the rest the same
+
+    public static void main(String[] args) {
+        shiftArrayElement(new int[]{1,2,3,4,5});
+    }
+
+
+    public static void shiftArrayElement(int[]array){
+       
+
+    }
 }
