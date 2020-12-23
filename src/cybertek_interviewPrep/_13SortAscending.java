e375881d569456f3468fcc6bf7e123cba8b0b7b9
Arasefe <ismail_yildirim69@yahoo.com>
2020-12-22 23:48:48 -0600

    22December2020

--- src/cybertek_interviewPrep/_13SortAscending.java
+++ src/cybertek_interviewPrep/_13SortAscending.java
@@ -1,35 +1,81 @@
 package cybertek_interviewPrep;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class _13SortAscending {
-
+    // create a method that sorts a int[] without using sort method
     public static void main(String[] args) {
         //sortAscending1();
-        sortAscending2();
+        //sortAscending2();
+        //sortAscending3();
+
+        sortDescending1();
     }
 
 
     public static void sortAscending1(){
-        ArrayList<Integer> list=new ArrayList<Integer>();
+       int[]arr=new int[]{121,23,432,54,65,76,234};
+        for (int i = 0; i < arr.length; i++) {
+            int temp=0;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]>arr[j]){
+                    temp=arr[i];        // 121
+                    arr[i]=arr[j];      // 23
+                    arr[j]=temp;        // 121
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
 
-        list.addAll(Arrays.asList(23,32,43,12,12,42,3,23,45,564,1211));
 
-        list.stream().sorted().forEach(System.out::println);
 
     }
 
-    public static void sortAscending2(){
-        ArrayList<Integer>aLst=new ArrayList<>();
-        aLst.addAll(Arrays.asList(12,3,13,4,545,34,64));
+    public static void sortAscending2() {
+        int[]arr=new int[]{13,12,32,43,64,768,45,24,1212};
 
+        for (int d = 0; d < arr.length; d++) {
+            int temp=0;
+            for (int f = d+1; f < arr.length ; f++) {
+                if(arr[d]>arr[f]){
+                    temp=arr[d];
+                    arr[d]=arr[f];
+                    arr[f]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
 
-        aLst.stream().sorted().forEach(System.out::println);
-        aLst.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
     }
 
+    public static void sortAscending3() {
+        int[] arr = new int[]{6,3,5,2,7,4,45,8,11};
+        for (int i = 0; i < arr.length; i++) {
+            int temp=0;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static void sortDescending1(){
+        int[] arr = new int[]{6,3,5,2,7,4,45,8,11};
+        for (int i = 0; i < arr.length; i++) {
+            int temp=0;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]<arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
 
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- src/cybertek_interviewPrep/_16PermutationCombinations.java
+++ src/cybertek_interviewPrep/_16PermutationCombinations.java
@@ -1,4 +1,23 @@
 package cybertek_interviewPrep;
 
+import java.util.Arrays;
+
 public class _16PermutationCombinations {
+
+
+    public static void main(String[] args) {
+        char[]ch=new char[]{'c','d','f'};
+        permutationMethod1(ch);
+    }
+
+    public static void permutationMethod1(char[]ch){
+        String[]arr=new String[ch.length*2];
+        int idx=0;
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = i+1; j < ch.length; j++) {
+                arr[idx]=""+ch[i]+ch[j];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
