83f393083e4a848f50e2fbbeea040dc002a4f450
Arasefe <ismail_yildirim69@yahoo.com>
2021-10-08 07:19:58 -0500

    08October2021

--- src/interviewPrep/SecondMaxMin.java
+++ src/interviewPrep/SecondMaxMin.java
@@ -2,8 +2,9 @@ package interviewPrep;
 
 public class SecondMaxMin {
     public static void main(String[] args) {
-        secondMax(new int[]{1,2,324,123,12,42});
-        secondMin(new int[]{1,2,324,123,12,42});
+//        secondMax(new int[]{1,2,324,123,12,42});
+//        secondMin(new int[]{1,2,324,123,12,42});
+        secondMin2(new int[]{1,2,324,123,12,42});
     }
 
     public static void secondMax(int[]numbers){
@@ -19,22 +20,43 @@ public class SecondMaxMin {
             }
 
         }
-        System.out.println(secondMax);
+        System.out.println("Second Max: "+secondMax);
+        System.out.println("Max: "+max);
     }
 
-    public static void secondMin(int[]numbers){
+    public static void secondMin(int[]numbers){         //4,6,8,2,1,9
 
-        int min = Integer.MAX_VALUE;
-        int secondMin = 0;
+        int min = Integer.MAX_VALUE;                    // 2B
+        int secondMin = 0;                              // 0
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < min) {
-                secondMin = min;
-                min = numbers[i];
+                secondMin = min;                        // secMin 2B
+                min = numbers[i];                       // min     4
             } else if (numbers[i] < secondMin) {
-                secondMin = numbers[i];
+                secondMin = numbers[i];                 // secMin 6
             }
 
         }
-        System.out.println(secondMin);
+        System.out.println("Second Min: "+secondMin);
+        System.out.println("Min: "+min);
     }
+
+    public static void secondMin2(int[]numbers){
+
+        int temp = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j <numbers.length ; j++) {
+                if(numbers[i]>numbers[j]){
+                    temp=numbers[i];
+                    numbers[i]=numbers[j];
+                    numbers[j]=temp;
+                }
+            }
+
+        }
+        System.out.println("Second Min: "+numbers[1]);
+        System.out.println("Min: "+numbers[0]);
+    }
+
 }
